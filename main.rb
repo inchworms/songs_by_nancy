bb992b815fc355b0fc008cce4445be60d77b15cd
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-07-01 18:18:33 +0200

    add a not_found.erb for 404 sites

--- main.rb
+++ main.rb
@@ -6,9 +6,24 @@ get '/' do
 end
 
 get '/about' do
+  @title = "All About This Website"
   erb :about
 end
 
 get '/contact' do
+  @title = "Contact"
   erb :contact
 end
+
+not_found do
+erb :not_found
+end
+
+get '/name/:x' do
+@name = params[:x]
+erb :show
+end
+
+__END__
+@@show
+<h1>Hello <%= @name %>!</h1>
